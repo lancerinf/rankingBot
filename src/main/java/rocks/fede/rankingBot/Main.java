321bec1af61c294eb0e83a4cb98b6b0b6d74cf02
Federico Lancerin <federico.lancerin@nordcloud.com>
2017-04-10 01:05:09 +0200

    Experiments with score prediction

--- src/main/java/rocks/fede/rankingBot/Main.java
+++ src/main/java/rocks/fede/rankingBot/Main.java
@@ -6,6 +6,13 @@ package rocks.fede.rankingBot;
 public class Main {
 
     public static void main(String[] args){
+        double myRank = 85;
+        double opponentRank = 15;
+        int TOP_SCORE = 5;
+        int predictedResult = TOP_SCORE - ScoreBot.predictScoreDifference(myRank,opponentRank,TOP_SCORE);
+        int manualPreResult = (int) (TOP_SCORE - Math.abs((myRank - opponentRank)/ ScoreBot.TOP_SCORE)*TOP_SCORE);
+        System.out.println("With my team at rank " + myRank + " and opponent at rank " + opponentRank + " the predicted result is " + TOP_SCORE + " - " + predictedResult);
+        System.out.println("With my team at rank " + myRank + " and opponent at rank " + opponentRank + " the manually predicted result is " + TOP_SCORE + " - " + manualPreResult);
 
     }
 }
--- src/main/java/rocks/fede/rankingBot/ScoreBot.java
+++ src/main/java/rocks/fede/rankingBot/ScoreBot.java
@@ -8,11 +8,17 @@ public class ScoreBot {
     public static final int TOP_SCORE = 100;
 
     public static double getUpdatedRank(double teamRank, double opponentsRank, int teamScore, int opponentScore) {
-        //TODO return updated Player rank that reflects latest match results.
+        //TODO return suggested rank adjustment to reflect latest results.
         return 0;
     };
 
+    public static int predictScoreDifference(double teamRank, double opponentsRank, int topScore) {
+        int predictedDifference = (int) Math.abs(((teamRank - opponentsRank)/ ScoreBot.TOP_SCORE)*topScore);
+        if (predictedDifference == 0) return 1;
+        else return predictedDifference;
+    }
+
     public static double getExpectationFactor(double teamRank, double opponentsRank) {
-        return Math.abs(teamRank - opponentsRank) / ScoreBot.TOP_SCORE;
+        return (teamRank - opponentsRank) / ScoreBot.TOP_SCORE;
     }
 }
