1602fcbf918210110e43ac75f70379e4569dcc98
Federico Lancerin <federico.lancerin@nordcloud.com>
2017-04-10 22:40:36 +0200

    Implemented predictLowerScore method that uses SND scores and boosts

--- src/main/java/rocks/fede/rankingBot/Main.java
+++ src/main/java/rocks/fede/rankingBot/Main.java
@@ -6,13 +6,19 @@ package rocks.fede.rankingBot;
 public class Main {
 
     public static void main(String[] args){
-        double myRank = 85;
-        double opponentRank = 15;
-        int TOP_SCORE = 5;
-        int predictedResult = TOP_SCORE - ScoreBot.predictScoreDifference(myRank,opponentRank,TOP_SCORE);
-        int manualPreResult = (int) (TOP_SCORE - Math.abs((myRank - opponentRank)/ ScoreBot.TOP_SCORE)*TOP_SCORE);
+        double myRank = 50;
+        double opponentRank = 20;
+        int TOP_SCORE = 25;
+        int predictedResult = ScoreBot.predictLowerScore(myRank,opponentRank,TOP_SCORE);
         System.out.println("With my team at rank " + myRank + " and opponent at rank " + opponentRank + " the predicted result is " + TOP_SCORE + " - " + predictedResult);
-        System.out.println("With my team at rank " + myRank + " and opponent at rank " + opponentRank + " the manually predicted result is " + TOP_SCORE + " - " + manualPreResult);
 
+//        double base = -3.0;
+//        double currentStep;
+//        for (int x = 0; x <= 60; x++) {
+//            currentStep = base + (x/10.0);
+//            System.out.println("SND (" + Math.round(currentStep*100)/100.00 + ") = " + ScoreBot.sndVal(currentStep));
+//        }
+
+        System.out.println("differenceAmplificationFactor = " + ScoreBot.getDifferenceAmplificationFactor(myRank,opponentRank) );
     }
 }
--- src/main/java/rocks/fede/rankingBot/ScoreBot.java
+++ src/main/java/rocks/fede/rankingBot/ScoreBot.java
@@ -5,20 +5,38 @@ package rocks.fede.rankingBot;
  */
 public class ScoreBot {
 
-    public static final int TOP_SCORE = 100;
+    public static final double TOP_SCORE = 100;
+
+    // NORMAL_EXPANSION is used to project a ratio onto a SND.
+    public static final double NORMAL_EXPANSION = 4.0;
+
+    // BOOST_AMPLIFICATION is used to add some weight to the getDifferenceAmplificationFactor method.
+    public static final double BOOST_AMPLIFICATION = 3.0;
 
     public static double getUpdatedRank(double teamRank, double opponentsRank, int teamScore, int opponentScore) {
         //TODO return suggested rank adjustment to reflect latest results.
         return 0;
     };
 
-    public static int predictScoreDifference(double teamRank, double opponentsRank, int topScore) {
-        int predictedDifference = (int) Math.abs(((teamRank - opponentsRank)/ ScoreBot.TOP_SCORE)*topScore);
-        if (predictedDifference == 0) return 1;
-        else return predictedDifference;
+    public static int predictLowerScore(double teamRank, double opponentsRank, int topScore) {
+        int predictedDifference = (int) Math.abs(((teamRank - opponentsRank)*(ScoreBot.getDifferenceAmplificationFactor(teamRank, opponentsRank))/ ScoreBot.TOP_SCORE)*topScore);
+        if (predictedDifference == 0) predictedDifference = 1;
+        return topScore - predictedDifference;
     }
 
-    public static double getExpectationFactor(double teamRank, double opponentsRank) {
-        return (teamRank - opponentsRank) / ScoreBot.TOP_SCORE;
+    /**
+     *
+     * @param teamRank
+     * @param opponentsRank
+     * @return Returns a multiplier to be used in "predictScoreDifference". This multiplier adds some difference when
+     *  the difference in ranking is little and adds close to nothing when the difference in ranking is high.
+     */
+    public static double getDifferenceAmplificationFactor(double teamRank, double opponentsRank) {
+        double normalizedRankDiff = ((teamRank - opponentsRank)/ScoreBot.TOP_SCORE)*ScoreBot.NORMAL_EXPANSION;
+        return (1.0 + Math.abs(sndVal(normalizedRankDiff)*ScoreBot.BOOST_AMPLIFICATION));
+    }
+
+    public static double sndVal(double x) {
+        return Math.pow(Math.E,(-(0.5)*Math.pow(x,2)))/Math.sqrt(2*Math.PI);
     }
 }
