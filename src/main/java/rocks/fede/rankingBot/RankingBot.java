40193e96120ce457e779911444946402689274e2
Federico Lancerin <federico.lancerin@schibsted.com>
2017-05-25 15:37:32 +0200

    Added RankingBot TODOs

--- src/main/java/rocks/fede/rankingBot/RankingBot.java
+++ src/main/java/rocks/fede/rankingBot/RankingBot.java
@@ -4,4 +4,6 @@ package rocks.fede.rankingBot;
  * Created by fede on 5/25/17.
  */
 public class RankingBot {
+    //TODO search for existing data
+    //TODO setup connection to Slack API
 }
--- src/main/java/rocks/fede/rankingBot/RankingBotMain.java
+++ src/main/java/rocks/fede/rankingBot/RankingBotMain.java
@@ -4,4 +4,6 @@ package rocks.fede.rankingBot;
  * Created by fede on 5/25/17.
  */
 public class RankingBotMain {
+    //TODO load parameters from properties file or environment
+    //TODO start an instance of RankingBot
 }
